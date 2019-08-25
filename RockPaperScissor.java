fd317c101bdd9347d91fcf0a4cb3e106574c573d
davmusko <davmusko@gmail.com>
2019-08-25 14:49:43 -0400

    game logic

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -12,7 +12,7 @@ public class RockPaperScissor {
 		String compPick;
 		int randNum = rGen.nextInt(3);
 		int userWins = 0;
-		int compterWins = 0;
+		int computerWins = 0;
 
 		// Welcome user to the app
 		System.out.println("Welcome to Roshambo!");
@@ -37,9 +37,37 @@ public class RockPaperScissor {
 		System.out.println(name + " picked " + userPick);
 		System.out.println("The computer picked " + compPick);
 
-		userWins++;
-		userWins += 1;
-		userWins = userWins + 1;
+		if(userPick.equalsIgnoreCase("rock")) {
+			if(compPick.equals("rock")) {
+				System.out.println("Tie");
+			} else if(compPick.equals("paper")) {
+				System.out.println("Computer Wins");
+				computerWins++;
+			} else {
+				System.out.println(name + " wins!");
+				userWins++;
+			}
+		} else if(userPick.equalsIgnoreCase("paper")) {
+			if(compPick.equals("rock")) {
+				System.out.println(name + " wins!");
+				userWins++;
+			} else if(compPick.equals("paper")) {
+				System.out.println("Tie");
+			} else {
+				System.out.println("Computer Wins");
+				computerWins++;
+			}
+		} else if(userPick.equalsIgnoreCase("scissors")) {
+			if(compPick.equals("rock")) {
+				System.out.println("Computer Wins");
+				computerWins++;
+			} else if(compPick.equals("paper")) {
+				System.out.println(name + " wins!");
+				userWins++;
+			} else {
+				System.out.println("Tie");
+			}
+		}
 
 		scan.close();
 	}
