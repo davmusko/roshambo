57344093f0ce1a5f49d38ecaf13c3e7aec1a2aab
davmusko <davmusko@gmail.com>
2019-08-25 17:05:12 -0400

    final

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -10,7 +10,8 @@ public class RockPaperScissor {
 		String name;
 		String userPick;
 		String compPick;
-		int randNum = rGen.nextInt(3);
+		String replay = "y";
+		int randNum;
 		int userWins = 0;
 		int computerWins = 0;
 
@@ -22,53 +23,63 @@ public class RockPaperScissor {
 		name = scan.nextLine();
 
 		System.out.println("Welcome " + name);
+		do {
+			randNum = getRandNum(rGen);
+			System.out.println("Please pick rock, paper, or scissors");
+			userPick = scan.next();
 
-		System.out.println("Please pick rock, paper, or scissors");
-		userPick = scan.nextLine();
-
-		if (randNum == 0) {
-			compPick = "rock";
-		} else if (randNum == 1) {
-			compPick = "paper";
-		} else {
-			compPick = "scissors";
-		}
-
-		System.out.println(name + " picked " + userPick);
-		System.out.println("The computer picked " + compPick);
-
-		if(userPick.equalsIgnoreCase("rock")) {
-			if(compPick.equals("rock")) {
-				System.out.println("Tie");
-			} else if(compPick.equals("paper")) {
-				System.out.println("Computer Wins");
-				computerWins++;
+			if (randNum == 0) {
+				compPick = "rock";
+			} else if (randNum == 1) {
+				compPick = "paper";
 			} else {
-				System.out.println(name + " wins!");
-				userWins++;
+				compPick = "scissors";
 			}
-		} else if(userPick.equalsIgnoreCase("paper")) {
-			if(compPick.equals("rock")) {
-				System.out.println(name + " wins!");
-				userWins++;
-			} else if(compPick.equals("paper")) {
-				System.out.println("Tie");
-			} else {
-				System.out.println("Computer Wins");
-				computerWins++;
-			}
-		} else if(userPick.equalsIgnoreCase("scissors")) {
-			if(compPick.equals("rock")) {
-				System.out.println("Computer Wins");
-				computerWins++;
-			} else if(compPick.equals("paper")) {
-				System.out.println(name + " wins!");
-				userWins++;
-			} else {
-				System.out.println("Tie");
-			}
-		}
 
+			System.out.println(name + " picked " + userPick);
+			System.out.println("The computer picked " + compPick);
+
+			if (userPick.equalsIgnoreCase("rock")) {
+				if (compPick.equals("rock")) {
+					System.out.println("Tie");
+				} else if (compPick.equals("paper")) {
+					System.out.println("Computer Wins");
+					computerWins++;
+				} else {
+					System.out.println(name + " wins!");
+					userWins++;
+				}
+			} else if (userPick.equalsIgnoreCase("paper")) {
+				if (compPick.equals("rock")) {
+					System.out.println(name + " wins!");
+					userWins++;
+				} else if (compPick.equals("paper")) {
+					System.out.println("Tie");
+				} else {
+					System.out.println("Computer Wins");
+					computerWins++;
+				}
+			} else if (userPick.equalsIgnoreCase("scissors")) {
+				if (compPick.equals("rock")) {
+					System.out.println("Computer Wins");
+					computerWins++;
+				} else if (compPick.equals("paper")) {
+					System.out.println(name + " wins!");
+					userWins++;
+				} else {
+					System.out.println("Tie");
+				}
+			} else {
+				System.out.println("invalid selection");
+				continue;
+			}
+			System.out.println("Would you like to play again? (y/n)");
+			replay = scan.next();
+		} while (replay.equalsIgnoreCase("y") || replay.equalsIgnoreCase("yes"));
 		scan.close();
+	}
+	public static int getRandNum(Random rGen) {
+		int randNum = rGen.nextInt(3);
+		return randNum;
 	}
 }
